5688fe80959e41c69d7384320d5dd2ec5dc47fcf
abrahham <valvdomero@gmail.com>
2021-11-01 22:50:40 -0600

    Métodos para canción

--- app/apis/apiCanciones.php
+++ app/apis/apiCanciones.php
@@ -17,10 +17,39 @@
 				? array("mensaje" => "Se ha agregado la canción.")
 				: array("mensaje" => "Ha habido problemas al tratar de registrar la canción.")
 			);
-		}
+		}		
 		public function mostrar() {
 			$arre = $this->modelo->getAll();
 			$this->encabezados("GET",false);
 			echo json_encode($arre);
+		}
+		public function ver($id) {
+			$data = json_encode(file_get_contents("php://input"),true);
+			$this->modelo->id = $id;
+			$arre = $this->modelo->get();
+			$this->encabezados("GET", false);
+			echo json_encode($arre);
+		}
+		public function eliminar($id) {
+			$this->modelo->id = $id;
+			$res = $this->modelo->delete();
+			$this->encabezados("DELETE", true);
+			echo json_encode(($res)
+				? array("mensaje" => "Se ha eliminado la canción.")
+				: array("mensaje" => "Ha habido problemas al tratar de eliminar la canción.")
+			);
+		}
+		public function cambiar() {
+			$data = json_decode(file_get_contents("php://input"),true);
+			$this->modelo->id = $data["id"];
+			$this->modelo->nombre = $data["nombre"];
+			$this->modelo->artista_id = $data["artistaId"];
+			$this->modelo->disco_id = $data["discoId"];
+			$res = $this->modelo->update();
+			$this->encabezados("PUT",true);
+			echo json_encode(($res)
+				? array("mensaje" => "Se ha modificado la canción.")
+				: array("mensaje" => "Ha habido problemas al tratar de modificar la canción.")
+			);
 		}
 	}
--- app/models/cancion.php
+++ app/models/cancion.php
@@ -20,12 +20,52 @@
 			$stm->bindParam(":disco",$this->disco_id);
 			return $stm->execute();
 		}
+		public function update() {
+			$dbcon = $this->db->connectar();
+			$query = "UPDATE {$this->tabla} SET nombre = :nombre, artista_id = :artista, disco_id = :disco WHERE id = :id";
+			$stm = $dbcon->prepare($query);
+			$stm->bindParam(":nombre",$this->nombre);
+			$stm->bindParam(":artista", $this->artista_id);
+			$stm->bindParam(":disco", $this->disco_id);
+			$stm->bindParam(":id", $this->id);
+			return $stm->execute();
+		}
+		public function delete() {
+			$dbcon = $this->db->connectar();
+			$query = "DELETE FROM {$this->tabla} WHERE id = :id";
+			$stm = $dbcon->prepare($query);
+			$stm->bindParam(":id", $this->id);
+			return $stm->execute();
+		}
+		public function get() {
+			$dbcon = $this->db->connectar();
+			$query = "SELECT id, nombre, artista_id, (SELECT nombre FROM disco WHERE id = ta.artista_id) AS anombre,"
+				."disco_id, (SELECT nombre FROM artista WHERE id = ta.artista_id) AS dnombre FROM {$this->tabla} AS ta"
+				." WHERE id = :id LIMIT 0,1";
+			$stm = $dbcon->prepare($query);
+			$stm->bindParam(":id",$this->id);
+			$stm->execute();
+			$arre = $stm->fetch(PDO::FETCH_ASSOC);
+			return array(
+				"id" => $arre["id"], "nombre" => $arre["nombre"],
+				"disco" => array("id" => $arre["disco_id"], "nombre" => $arre["dnombre"]),
+				"artista" => array("id" => $arre["artista_id"], "nombre" => $arre["anombre"])	
+			);
+		}
 		public function getAll() {
 			$dbcon = $this->db->connectar();
-			$query = "SELECT id,nombre,artista_id,disco_id FROM {$this->tabla}";
+			$query = "SELECT id, nombre, disco_id, (SELECT nombre FROM disco WHERE id = ta.disco_id) AS dnombre,"
+				."artista_id, (SELECT nombre FROM artista WHERE id = ta.artista_id) AS anombre FROM {$this->tabla} AS ta";
 			$stm = $dbcon->prepare($query);
 			$stm->execute();
-			$arre = $stm->fetchAll(PDO::FETCH_ASSOC);
+			$arre = [];
+			while($data = $stm->fetch(PDO::FETCH_ASSOC)) {
+				array_push($arre,array(
+					"id" => $data["id"], "nombre" => $data["nombre"],
+					"disco" => array("id" => $data["disco_id"], "nombre" => $data["dnombre"]),
+					"artista" => array("id" => $data["artista_id"], "nombre" => $data["anombre"])	
+				));
+			}
 			return $arre;
 		}
-	}
+	}	
